29499087d6e0d9be56423ffd8486d1fe5f384c6f
thibcott <thibcott@outlook.com>
2025-11-21 10:15:52 +0100

    cleanup: remove unused commented code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,29 +16,3 @@ export default defineConfig({
     },
   },
 })
-
-// import { fileURLToPath, URL } from 'node:url'
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url)),
-//     },
-//   },
-//   server: {
-//     proxy: {
-//       // Tout ce qui commence par /api sera redirigé vers le backend Symfony
-//       '/api': {
-//         target: 'http://localhost:8000',
-//         changeOrigin: true,
-//         secure: false,
-//         // IMPORTANT : on enlève le prefix /api pour que Symfony voie /trips
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       },
-//     },
-//   },
-// })
