465abc8dd53084bc5e968a7730e2fcce873e10c2
thibcott <thibcott@outlook.com>
2025-11-24 17:44:11 +0100

    feat: add ESLint configuration for TypeScript files

---
+++ frontend/eslint.config.cjs
@@ -0,0 +1,15 @@
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    rules: {},
+  },
+];
